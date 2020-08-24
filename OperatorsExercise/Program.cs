c14dc76088226039a058f8653155d29770511be9
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-24 14:28:41 -0500

    master/answer branch

--- OperatorsExercise/Program.cs
+++ OperatorsExercise/Program.cs
@@ -6,34 +6,9 @@ namespace OperatorsExercise
     {
         static void Main(string[] args)
         {
-          //explicit typing -
-          string variableName = "Hello";
-          //inferred typing - best practice
-          var varName = "Hello";
-
-            Console.WriteLine($"{variableName}");
-
-            var a = 17;
-            var b = 4;
-
-            var div = a / b;
-            var mod = a % b;
-
-            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
-
-            //Allow user to input the radius
-            Console.WriteLine("Input your circles radius to find the area");
-            var radius = double.Parse(Console.ReadLine());
-
-            //Calculate the area of the circle
-            var areaOfCircle = CalculateArea(radius);
-
-            Console.WriteLine($"The area of a circle with a radius of {radius} is {areaOfCircle}");
+         
         }
 
-        public static double CalculateArea(double radius)
-        {
-            return Math.PI * (radius * radius);
-        }
+       
     }
 }
