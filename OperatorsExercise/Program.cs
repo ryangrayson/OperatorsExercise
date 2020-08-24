d7e939104116c4eeea7c5612af12dd6af0e3d4b2
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-24 12:50:53 -0500

    First equation

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //explicit typing -
+          string variableName = "Hello";
+          //inferred typing - best practice
+          var varName = "Hello";
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+        }
+    }
+}
